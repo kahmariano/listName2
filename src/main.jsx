7deeb09fc368f28ffded9739bb34826d91563f34
Karina Mariano <kmariano138@gmail.com>
2023-05-11 14:22:45 -0300

    first commit1

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/Home/index.jsx'
+import './styles/global.css'
+ 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+//arquivo que carega a pag
+//pq a importação de stylos veio aki?
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+<h1>lista de nomes</h1>
+    </>
+  )
+}
+
+export default App
