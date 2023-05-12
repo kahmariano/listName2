ee69cc0397b3d4604d016373570b9682f54af9cc
Karina Mariano <kmariano138@gmail.com>
2023-05-11 22:15:54 -0300

    components

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,15 @@
+export function Card () {
+    return(
+        <div className="card">
+            <strong></strong>
+            <small></small>
+        </div>
+
+    )
+}
+
+//Card é um componente, componentes sao codigos independentes que podem ser reutilizados. geralmente uma function que retorna html
+
+// Props: dentro dos components temos as props que sao propriedades, ou seja, um objeto com informações
+
+//state: estado, é criado dentro do componente e é mutavel, por exemplo um botao esta ativado ou nao, se revolve com estado
